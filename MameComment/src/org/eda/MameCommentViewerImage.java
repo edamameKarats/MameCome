1fdf61325dad86e2e6ec560051ac7ac228d7d463
edamameKarats <39410248+edamameKarats@users.noreply.github.com>
2019-10-22 15:33:12 +0900

    Exceptionを潰していたのでStackTraceが出るように修正

--- MameComment/src/org/eda/MameCommentViewerImage.java
+++ MameComment/src/org/eda/MameCommentViewerImage.java
@@ -53,6 +53,7 @@ public class MameCommentViewerImage {
 				fileName=Paths.get("cachedImages/"+tmpFileName).toRealPath(LinkOption.NOFOLLOW_LINKS).toString();
 				aImageMap.put(aFileUrl, fileName);
 			}catch(Exception e) {
+				e.printStackTrace();
 				//エラーが起きた場合はblankファイルを使用するようにする。
 				//次回もトライするので、Mapは更新しない
 				writeDebugLog("some exception is occured during file get and save");
